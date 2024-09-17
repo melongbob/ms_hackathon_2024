d9e6a738b61b0dd1daa5a3e3f544dc04d1ec78b6
Yun Kim <yunkim@microsoft.com>
2024-09-17 13:26:36 -0700

    add right side landmarks

--- human_posture_analysis_video.py
+++ human_posture_analysis_video.py
@@ -93,6 +93,100 @@ if __name__ == "__main__":
         # Acquire the landmark coordinates.
         # Once aligned properly, left or right should not be a concern.      
         # Left shoulder.
+        landmark = {
+            "LEFT": {
+                "EAR": {
+                    "x": int(lm.landmark[lmPose.LEFT_EAR].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_EAR].y * h)
+                },
+                "SHOULDER": {
+                    "x": int(lm.landmark[lmPose.LEFT_SHOULDER].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_SHOULDER].y * h)
+                },
+                "ELBOW": {
+                    "x": int(lm.landmark[lmPose.LEFT_ELBOW].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_ELBOW].y * h)
+                },
+                "WRIST": {
+                    "x": int(lm.landmark[lmPose.LEFT_ELBOW].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_ELBOW].y * h)
+                },
+                "ELBOW": {
+                    "x": int(lm.landmark[lmPose.LEFT_ELBOW].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_ELBOW].y * h)
+                },
+                "WRIST": {
+                    "x": int(lm.landmark[lmPose.LEFT_WRIST].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_WRIST].y * h)
+                },
+                "INDEX": {
+                    "x": int(lm.landmark[lmPose.LEFT_INDEX].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_INDEX].y * h)
+                },
+                "HIP": {
+                    "x": int(lm.landmark[lmPose.LEFT_HIP].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_HIP].y * h)
+                },
+                "KNEE": {
+                    "x": int(lm.landmark[lmPose.LEFT_KNEE].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_KNEE].y * h)
+                },
+                "HEEL": {
+                    "x": int(lm.landmark[lmPose.LEFT_HEEL].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_HEEL].y * h)
+                },
+                "FOOT_INDEX": {
+                    "x": int(lm.landmark[lmPose.LEFT_FOOT_INDEX].x * w), 
+                    "y": int(lm.landmark[lmPose.LEFT_FOOT_INDEX].y * h)
+                }
+            },
+            "RIGHT": {
+                "EAR": {
+                    "x": int(lm.landmark[lmPose.RIGHT_EAR].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_EAR].y * h)
+                },
+                "SHOULDER": {
+                    "x": int(lm.landmark[lmPose.RIGHT_SHOULDER].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
+                },
+                "ELBOW": {
+                    "x": int(lm.landmark[lmPose.RIGHT_ELBOW].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_ELBOW].y * h)
+                },
+                "WRIST": {
+                    "x": int(lm.landmark[lmPose.RIGHT_ELBOW].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_ELBOW].y * h)
+                },
+                "ELBOW": {
+                    "x": int(lm.landmark[lmPose.RIGHT_ELBOW].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_ELBOW].y * h)
+                },
+                "WRIST": {
+                    "x": int(lm.landmark[lmPose.RIGHT_WRIST].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_WRIST].y * h)
+                },
+                "INDEX": {
+                    "x": int(lm.landmark[lmPose.RIGHT_INDEX].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_INDEX].y * h)
+                },
+                "HIP": {
+                    "x": int(lm.landmark[lmPose.RIGHT_HIP].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_HIP].y * h)
+                },
+                "KNEE": {
+                    "x": int(lm.landmark[lmPose.RIGHT_KNEE].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_KNEE].y * h)
+                },
+                "HEEL": {
+                    "x": int(lm.landmark[lmPose.RIGHT_HEEL].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_HEEL].y * h)
+                },
+                "FOOT_INDEX": {
+                    "x": int(lm.landmark[lmPose.RIGHT_FOOT_INDEX].x * w), 
+                    "y": int(lm.landmark[lmPose.RIGHT_FOOT_INDEX].y * h)
+                }
+            }
+        }
         l_shldr_x = int(lm.landmark[lmPose.LEFT_SHOULDER].x * w)
         l_shldr_y = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h)
         # Right shoulder
@@ -121,7 +215,8 @@ if __name__ == "__main__":
         l_ankle_y = int(lm.landmark[lmPose.LEFT_ANKLE].y * h)
 
         # Calculate distance between left shoulder and right shoulder points.
-        offset = findDistance(l_shldr_x, l_shldr_y, r_shldr_x, r_shldr_y)
+        # offset = findDistance(l_shldr_x, l_shldr_y, r_shldr_x, r_shldr_y)
+        offset = findDistance(landmark["LEFT"]["SHOULDER"]["x"], landmark["LEFT"]["SHOULDER"]["y"], landmark["RIGHT"]["SHOULDER"]["x"], landmark["RIGHT"]["SHOULDER"]["y"])
 
         # Assist to align the camera to point at the side view of the person.
         # Offset threshold 30 is based on results obtained from analysis over 100 samples.
@@ -131,33 +226,54 @@ if __name__ == "__main__":
             cv2.putText(image, str(int(offset)) + ' Not Aligned', (w - 150, 30), font, 0.9, red, 2)
 
         # Calculate angles.
-        neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
-        torso_inclination = findAngle(l_hip_x, l_hip_y, l_shldr_x, l_shldr_y)
-        thigh_inclination = findAngle(l_hip_x, l_hip_y, l_knee_x, l_knee_y)
-        calf_inclination = findAngle(l_knee_x, l_knee_y, l_ankle_x, l_ankle_y)
-        upper_arm_inclination = findAngle(l_shldr_x, l_shldr_y, l_elbow_x, l_elbow_y)
-        lower_arm_inclination = findAngle(l_elbow_x, l_elbow_y, l_wrist_x, l_wrist_y)
+        # neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
+        # torso_inclination = findAngle(l_hip_x, l_hip_y, l_shldr_x, l_shldr_y)
+        # thigh_inclination = findAngle(l_hip_x, l_hip_y, l_knee_x, l_knee_y)
+        # calf_inclination = findAngle(l_knee_x, l_knee_y, l_ankle_x, l_ankle_y)
+        # upper_arm_inclination = findAngle(l_shldr_x, l_shldr_y, l_elbow_x, l_elbow_y)
+        # lower_arm_inclination = findAngle(l_elbow_x, l_elbow_y, l_wrist_x, l_wrist_y)
+        neck_inclination = findAngle(landmark["LEFT"]["SHOULDER"]["x"], landmark["LEFT"]["SHOULDER"]["y"], landmark["LEFT"]["EAR"]["x"], landmark["LEFT"]["EAR"]["y"])
+        torso_inclination = findAngle(landmark["LEFT"]["HIP"]["x"], landmark["LEFT"]["HIP"]["y"], landmark["LEFT"]["SHOULDER"]["x"], landmark["LEFT"]["SHOULDER"]["y"])
+        thigh_inclination = findAngle(landmark["LEFT"]["HIP"]["x"], landmark["LEFT"]["HIP"]["y"], landmark["LEFT"]["KNEE"]["x"], landmark["LEFT"]["KNEE"]["y"])
+        calf_inclination = findAngle(landmark["LEFT"]["KNEE"]["x"], landmark["LEFT"]["KNEE"]["y"], landmark["LEFT"]["HEEL"]["x"], landmark["LEFT"]["HEEL"]["y"])
+        upper_arm_inclination = findAngle(landmark["LEFT"]["SHOULDER"]["x"], landmark["LEFT"]["SHOULDER"]["y"], landmark["LEFT"]["ELBOW"]["x"], landmark["LEFT"]["ELBOW"]["y"])
+        lower_arm_inclination = findAngle(landmark["LEFT"]["ELBOW"]["x"], landmark["LEFT"]["ELBOW"]["y"], landmark["LEFT"]["WRIST"]["x"], landmark["LEFT"]["WRIST"]["y"])
 
         # Draw landmarks.
         cv2.circle(image, (l_shldr_x, l_shldr_y), 7, yellow, -1)
         cv2.circle(image, (l_ear_x, l_ear_y), 7, yellow, -1)
+        cv2.circle(image, (r_shldr_x, r_shldr_y), 7, pink, -1)
+        cv2.circle(image, (landmark["RIGHT"]["EAR"]["x"], landmark["RIGHT"]["EAR"]["y"]), 7, pink, -1)
 
         # Let's take y - coordinate of P3 100px above x1,  for display elegance.
         # Although we are taking y = 0 while calculating angle between P1,P2,P3.
-        cv2.circle(image, (l_shldr_x, l_shldr_y - 100), 7, yellow, -1)
-        cv2.circle(image, (r_shldr_x, r_shldr_y), 7, pink, -1)
+        # cv2.circle(image, (l_shldr_x, l_shldr_y - 100), 7, yellow, -1)
+        # cv2.circle(image, (r_shldr_x, r_shldr_y), 7, pink, -1)
         cv2.circle(image, (l_hip_x, l_hip_y), 7, yellow, -1)
-
+        cv2.circle(image, (r_hip_x, r_hip_y), 7, pink, -1)
+        
         # Similarly, here we are taking y - coordinate 100px above x1. Note that
         # you can take any value for y, not necessarily 100 or 200 pixels.
-        cv2.circle(image, (l_hip_x, l_hip_y - 100), 7, yellow, -1)
-        cv2.circle(image, (r_hip_x, r_hip_y), 7, pink, -1)
+        # cv2.circle(image, (l_hip_x, l_hip_y - 100), 7, yellow, -1)
         cv2.circle(image, (l_knee_x, l_knee_y), 7, yellow, -1)
-        cv2.circle(image, (l_ankle_x, l_ankle_y), 7, yellow, -1)
+        cv2.circle(image, (landmark["RIGHT"]["KNEE"]["x"], landmark["RIGHT"]["KNEE"]["y"]), 7, pink, -1)
+        # cv2.circle(image, (l_ankle_x, l_ankle_y), 7, yellow, -1)
+        cv2.circle(image, (landmark["LEFT"]["HEEL"]["x"], landmark["LEFT"]["HEEL"]["y"]), 7, yellow, -1)
+        cv2.circle(image, (landmark["RIGHT"]["HEEL"]["x"], landmark["RIGHT"]["HEEL"]["y"]), 7, pink, -1)
 
         # arms
         cv2.circle(image, (l_elbow_x, l_elbow_y), 7, yellow, -1)
+        cv2.circle(image, (landmark["RIGHT"]["ELBOW"]["x"], landmark["RIGHT"]["ELBOW"]["y"]), 7, pink, -1)
         cv2.circle(image, (l_wrist_x, l_wrist_y), 7, yellow, -1)
+        cv2.circle(image, (landmark["RIGHT"]["WRIST"]["x"], landmark["RIGHT"]["WRIST"]["y"]), 7, pink, -1)
+
+        # feet
+        cv2.circle(image, (landmark["LEFT"]["FOOT_INDEX"]["x"], landmark["LEFT"]["FOOT_INDEX"]["y"]), 7, yellow, -1)
+        cv2.circle(image, (landmark["RIGHT"]["FOOT_INDEX"]["x"], landmark["RIGHT"]["FOOT_INDEX"]["y"]), 7, pink, -1)
+
+        # finger
+        cv2.circle(image, (landmark["LEFT"]["INDEX"]["x"], landmark["LEFT"]["INDEX"]["y"]), 7, yellow, -1)
+        cv2.circle(image, (landmark["RIGHT"]["INDEX"]["x"], landmark["RIGHT"]["INDEX"]["y"]), 7, pink, -1)
 
         # Put text, Posture and angle inclination.
         # Text string for display.
@@ -182,7 +298,8 @@ if __name__ == "__main__":
             cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(thigh_inclination)), (l_knee_x + 10, l_knee_y), font, 0.9, light_green, 2)
-            cv2.putText(image, str(int(calf_inclination)), (l_ankle_x + 10, l_ankle_y), font, 0.9, light_green, 2)
+            # cv2.putText(image, str(int(calf_inclination)), (l_ankle_x + 10, l_ankle_y), font, 0.9, light_green, 2)
+            cv2.putText(image, str(int(calf_inclination)), (landmark["LEFT"]["HEEL"]["x"] + 10, landmark["LEFT"]["HEEL"]["y"]), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(upper_arm_inclination)), (l_elbow_x + 10, l_elbow_y), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(lower_arm_inclination)), (l_wrist_x + 10, l_wrist_y), font, 0.9, light_green, 2)
 
@@ -192,7 +309,8 @@ if __name__ == "__main__":
             cv2.line(image, (l_hip_x, l_hip_y), (l_shldr_x, l_shldr_y), green, 4)
             cv2.line(image, (l_hip_x, l_hip_y), (l_hip_x, l_hip_y - 100), green, 4)
             cv2.line(image, (l_knee_x, l_knee_y), (l_hip_x, l_hip_y), green, 4)
-            cv2.line(image, (l_ankle_x, l_ankle_y), (l_knee_x, l_knee_y), green, 4)
+            # cv2.line(image, (l_ankle_x, l_ankle_y), (l_knee_x, l_knee_y), green, 4)
+            cv2.line(image, (landmark["LEFT"]["HEEL"]["x"], landmark["LEFT"]["HEEL"]["y"]), (l_knee_x, l_knee_y), green, 4)
             cv2.line(image, (l_elbow_x, l_elbow_y), (l_shldr_x, l_shldr_y), green, 4)
             cv2.line(image, (l_elbow_x, l_elbow_y), (l_wrist_x, l_wrist_y), green, 4)
 
@@ -206,7 +324,8 @@ if __name__ == "__main__":
             cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
             cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, red, 2)
             cv2.putText(image, str(int(thigh_inclination)), (l_knee_x + 10, l_knee_y), font, 0.9, red, 2)
-            cv2.putText(image, str(int(calf_inclination)), (l_ankle_x + 10, l_ankle_y), font, 0.9, red, 2)
+            # cv2.putText(image, str(int(calf_inclination)), (l_ankle_x + 10, l_ankle_y), font, 0.9, red, 2)
+            cv2.putText(image, str(int(calf_inclination)), (landmark["LEFT"]["HEEL"]["x"] + 10, landmark["LEFT"]["HEEL"]["y"]), font, 0.9, red, 2)
             cv2.putText(image, str(int(upper_arm_inclination)), (l_elbow_x + 10, l_elbow_y), font, 0.9, red, 2)
             cv2.putText(image, str(int(lower_arm_inclination)), (l_wrist_x + 10, l_wrist_y), font, 0.9, red, 2)
 
@@ -216,7 +335,8 @@ if __name__ == "__main__":
             cv2.line(image, (l_hip_x, l_hip_y), (l_shldr_x, l_shldr_y), red, 4)
             cv2.line(image, (l_hip_x, l_hip_y), (l_hip_x, l_hip_y - 100), red, 4)
             cv2.line(image, (l_knee_x, l_knee_y), (l_hip_x, l_hip_y), red, 4)
-            cv2.line(image, (l_ankle_x, l_ankle_y), (l_knee_x, l_knee_y), red, 4)
+            # cv2.line(image, (l_ankle_x, l_ankle_y), (l_knee_x, l_knee_y), red, 4)
+            cv2.line(image, (landmark["LEFT"]["HEEL"]["x"], landmark["LEFT"]["HEEL"]["y"]), (l_knee_x, l_knee_y), red, 4)
             cv2.line(image, (l_elbow_x, l_elbow_y), (l_shldr_x, l_shldr_y), red, 4)
             cv2.line(image, (l_elbow_x, l_elbow_y), (l_wrist_x, l_wrist_y), red, 4)
 
